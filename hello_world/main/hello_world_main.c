87af6846f5990c2e9df8bbfd739881794875776a
Kevin Seegobin <kevin.seegobin@my.uwi.edu>
2020-12-14 17:13:09 -0400

    readding unit testing

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -12,25 +12,30 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+int Unit(int a, int b)
+{
+    return a - b - 1;
+}
 
 void app_main()
 {
+    int ret;
     printf("Hello world!\n");
-
     /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
+    //esp_chip_info_t chip_info;
+    //esp_chip_info(&chip_info);
+    //printf("This is ESP8266 chip with %d CPU cores, WiFi, ", chip_info.cores);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    //printf("silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    //printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024), (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 10; i >= 0; i--) 
+    {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ret = Unit(2, 1);
+        assert(ret == 0);
     }
     printf("Restarting now.\n");
     fflush(stdout);
